dc0b4f1d3cb8e56027746469ee963ac1a8e35e42
josephineAr <josephine.arinda@emtechhouse.com>
2026-01-23 16:04:28 +0300

    added login auth

--- auth.js
+++ auth.js
@@ -3,14 +3,14 @@ const bcrypt = require('bcrypt');  //for hashing passwords
 const pool =require('../db');  //the database Connection.used to run SQL querres
 
 
-const router = express.Router();  //helps defne routes separately from index.js
+const router = express.Router();  //helps define routes separately from index.js
 
-router.post('/signup',async (req, res)) // endpoint
-
+router.post('/signup',async (req, res) => {// endpoint
+try{
 const {name, email, password} = req.body; //get data from the request
 
 //checking if user exists
-const user = await pool.query('SELECT * FROM users WHERE email = $1' , [email]);
+const user = await pool.query('SELECT * FROM users WHERE email = $1' , [email]); //pool.query allows you to run SQL commands
 if (user.rows.length > 0){
     return res.status(400).json({message: 'User already exists'});
 
@@ -29,6 +29,45 @@ if (user.rows.length > 0){
         id:newUser.rows[0].id,
         name:newUser.rows[0].name,
         email:newUser.rows[0].email
-
     }
+    });
+   }catch (err){
+    console.error(err.message);
+    res.status(500).json({error:"server error"})
+   }
  });
+
+ //LOGIN
+ router.post("/login", async (req, res) => {
+    try{
+       const {email, password} = req.body;
+       //check if user is there
+       const user = await pool.query(
+        "SELECT * FROM users WHERE email = $1",[email]
+       );
+        if (user.rows.length === 0){
+            return res.status(401).json({message:"Invalid credentials"});
+ 
+        }
+        //check if passwords match
+        const validPassword = await bcrypt.compare(
+            password,
+            user.rows[0].password
+        );
+        if (!validPassword){
+            return res.status(401).json({message:"invalid credentials"});
+        }
+        res.json({
+            message:"Login successful",
+            user:{
+                id:user.rows[0].id,
+                name:user.rows[0].name,
+                email:user.rows[0].email,
+            }
+        })
+    }catch(err){
+        console.error(err);
+        res.status(500).json({message:"server error"});
+    }
+ })
+ module.exports =router;
