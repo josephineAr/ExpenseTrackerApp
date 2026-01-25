94c5659e6cbcecb7bed7fad93af90c0fc72ca3aa
josephineAr <josephine.arinda@emtechhouse.com>
2026-01-25 23:57:46 +0300

    updated routes

--- main.dart
+++ main.dart
@@ -8,6 +8,7 @@ import 'transaction.dart';
 import 'splashscreen.dart';
 import 'login.dart';
 import 'navbar.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -21,35 +22,43 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   ThemeMode _mode = ThemeMode.system;
-   void setThemeMode(ThemeMode newMode) {
+
+  void setThemeMode(ThemeMode newMode) {
     setState(() => _mode = newMode);
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme:ThemeData.light(),
-      darkTheme:ThemeData.dark(),
-      themeMode:_mode ,
-     
+      title: 'TrackFunds',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        brightness: Brightness.light,
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        useMaterial3: true,
+      ),
+      themeMode: _mode,
       debugShowCheckedModeBanner: false,
-      routes:{
-        '/settings':(context)=>Settings(
-          currentMode: _mode,
-          onModeChanged: setThemeMode
-        ),
-        '/sign':(context)=>Sign(),
-        '/home':(context)=>Home(),
-        '/debt':(context)=>Debt(),
-        '/transaction':(context)=>AddTransaction(),
-        '/about':(context)=>About(),
-        '/login':(context)=>login(),
-        '/navbar':(context)=>Navbar()
+      
+      
+      routes: {
+        '/settings': (context) => Settings(
+              currentMode: _mode,
+              onModeChanged: setThemeMode,
+            ),
+        '/sign': (context) =>  Sign(),
+        '/home': (context) => Home(),
+        '/debt': (context) =>  Debt(),
+        '/about': (context) => const About(),
+        '/login': (context) => login(), 
       },
-       home:Splashscreen(),
+      
+      
+      
+      home: const Splashscreen(),
     );
   }
 }
-
-
-
-
